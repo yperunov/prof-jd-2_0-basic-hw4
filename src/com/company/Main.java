d6086f63c618de4b385c4aa2cca6eef45a858316
yperunov <perunovyuri@gmail.com>
2021-11-26 19:24:30 +0300

    Подготовил решение 3 задач

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        // task1
+        int countUp = 1;
+        while (countUp <= 10) {
+            System.out.println(countUp);
+            countUp++;
+        }
+        System.out.println(" ");
+        for (int countDown = 10; countDown > 0; countDown--) {
+            System.out.println(countDown);
+        }
+
+        //task2
+        int friday = 5;
+        for (; friday<=31;friday=friday+7) {
+            System.out.println("Сегодня пятница, "+friday+" число. Необходимо подготовить отчет");
+        }
+
+        //task3
+        int yearStart = 2021-200;
+        int yearFinish = 2021+100;
+        for (int c=0; c<yearFinish;c=c+79){
+            if (c>yearStart)
+            System.out.println(c);
+        }
     }
 }
